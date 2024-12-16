273a1fe251506cba77beda40b7c7d5319c5bada8
Dhimmar Smit <smitd1074@gmail.com>
2024-12-16 18:35:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-function swiper(){
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        loop: true,
-        autoplay: {
-            delay: 7500,
-            disableOnInteraction: false,
-          },
-    });
-}swiper();
+var swiper1 = new Swiper(".mySwiper1", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+    },
+});
+
+// Swiper 2 Configuration
+var swiper2 = new Swiper(".mySwiper2", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
 
 var menuBar = document.querySelector('.menu-bar');
 var closeIcon = document.querySelector('.menu-bar i');
