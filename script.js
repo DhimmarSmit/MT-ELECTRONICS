998ab8b8627c9a7bd6b142d8293a6db7121acbd6
Dhimmar Smit <smitd1074@gmail.com>
2024-12-16 11:41:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function swiper(){
           },
     });
 }swiper();
+
+var menuBar = document.querySelector('.menu-bar');
+var closeIcon = document.querySelector('.menu-bar i');
+var barsIcon = document.querySelector('.fa-bars');
+
+// Show menu
+barsIcon.addEventListener('click', () => {
+    menuBar.classList.add('show');
+});
+
+// Hide menu
+closeIcon.addEventListener('click', () => {
+    menuBar.classList.remove('show');
+});
